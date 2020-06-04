313056d6f703dbe659c55643c6b8ee4701100023
motofi <jessicamallred@gmail.com>
2020-06-04 19:02:57 -0400

    work on flask app

--- app.py
+++ app.py
@@ -5,17 +5,32 @@
 # or
 # 127.0.0.1:5000
 # in browser should take me to the page
-
+# %matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
 # 1. import Flask - says that we're creating a Flask app, differentiating it from a Python app
 from flask import Flask, jsonify
 
 # Set up database
 # 1- connect to sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # 2- relect the db
+Base = automap_base()
 # 3 - reflect the tables
+Base.prepare(engine, reflect=True)
 # 4 - name the classes for reference
-
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+# 5 - Create a session
+session = Session(engine)
 
 # Set up Flask
 # 2. Create an app, being sure to pass __name__
@@ -23,6 +38,7 @@ app = Flask(__name__)
 
 # 3. Home Page
 # Define what to do when a user hits the index route
+# List all the routes that are available
 @app.route("/")
 def index():
     "List all available api routes."
@@ -35,11 +51,15 @@ def index():
     f"/api/v1.0/<start>/<end><br/>"
     )
 
-# List all the routes that are available
-#
+
 # @app.route("/api/v1.0/precipitation")
 # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-#
+last_date = dt.datetime(2017, 8, 23)
+last_twelve_mos = last_date - dt.timedelta(days = 365)
+twelve_mos_precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_twelve_mos).all()
+
+
+
 # Return the JSON representation of your dictionary.
 
 # @app.route("/api/v1.0/stations")
