d31a4aaa0163ae9e84f83962cc73701458c5b121
motofi <jessicamallred@gmail.com>
2020-06-08 20:51:41 -0400

    Push before final cleanup

--- app.py
+++ app.py
@@ -24,13 +24,15 @@ from flask import Flask, jsonify
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # 2- relect the db
 Base = automap_base()
-# 3 - reflect the tables
+# 3 - reflect the
 Base.prepare(engine, reflect=True)
 # 4 - name the classes for reference
 Station = Base.classes.station
 Measurement = Base.classes.measurement
-# 5 - Create a session
+
+# Create our session (link) from Python to the DB
 session = Session(engine)
+
 
 # Set up Flask
 # 2. Create an app, being sure to pass __name__
@@ -52,25 +54,41 @@ def index():
     )
 
 
-# @app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    f"The last twelve months of Hawaii precipitation data."
 # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-last_date = dt.datetime(2017, 8, 23)
-last_twelve_mos = last_date - dt.timedelta(days = 365)
-twelve_mos_precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_twelve_mos).all()
+    # last_date = dt.datetime(2017, 8, 23)
+    last_twelve_mos = dt.datetime(2017, 8, 23) - dt.timedelta(days = 365)
+    twelve_mos_precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_twelve_mos).all()
+
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    precip = {date: prcp for date, prcp in twelve_mos_precip}
+    return jsonify(precip)
 
 
 
-# Return the JSON representation of your dictionary.
+    # session.close()
 
-# @app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/stations")
+def stations():
+    f"Return a JSON list of stations from the dataset."
 # Return a JSON list of stations from the dataset.
+    station_list = session.query(Station.station).all()
+    return jsonify(station_list)
 
-# @app.route("/api/v1.0/tobs")
-# Query the dates and temperature observations of the most active station for the last year of data.
-#
-# Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def active_st_tobs():
+    f"Query the dates and temperature observations of the most active station \
+    for the last year of data. Return a JSON list of temperature observations \
+    (TOBS) for the previous year."
+    last_twelve_mos = dt.datetime(2017, 8, 23) - dt.timedelta(days = 365)
+    twelve_mos_temps = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= last_twelve_mos).all()
+    return jsonify(twelve_mos_temps)
+
 
 # @app.route("/api/v1.0/<start>")
+
 #  Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 #
 #  When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
@@ -83,10 +101,10 @@ twelve_mos_precip = session.query(Measurement.date, Measurement.prcp).filter(Mea
 
 
 # 4. Define what to do when a user hits the /api/v1.0/precipitation route
-@app.route("/api/v1.0/precipitation")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+# @app.route("/api/v1.0/precipitation")
+# def about():
+#     print("Server received request for 'About' page...")
+#     return "Welcome to my 'About' page!"
 
 # Keeps the Flask app running
 if __name__ == "__main__":
